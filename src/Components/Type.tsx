00648a7ae75c1f79fc0b4bbd878275eebfaeed0c
pagadalasreenivas <sreenivaspagadala1999@gmail.com>
2024-12-24 15:56:33 -0600

    Vercel changes

--- src/Components/Type.tsx
+++ src/Components/Type.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import TypewriterComponent from "typewriter-effect";
 export default function Type(){
     return(
-        <TypewriterComponent  options={{strings:["Software Developer","Mern Stack"]
+        <TypewriterComponent  options={{strings:["Software Developer","Java and React Full-Stack Developer"]
             ,autoStart:true,loop:true,deleteSpeed:50}} />
     );
 }
